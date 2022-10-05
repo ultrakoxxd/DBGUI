49760a0ac01c3ecf8b25e9333dc3fb6dfd90cf48
ultrakox <pawel.minkwitz@onet.pl>
2022-10-05 19:38:51 +0200

    test

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.ultrakox.dbgui.dbgui {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.ultrakox.dbgui.dbgui to javafx.fxml;
+    exports com.ultrakox.dbgui.dbgui;
+}
